ba16fd924335b44e425e27a2d1fbe73d6fa7abe4
luoqiangwei <luoqiangwei@live.cn>
2018-08-11 18:57:08 +0800

    write README file

--- README.md
+++ README.md
@@ -1,7 +1,27 @@
 # 奇点工作室网站
 
-使用工具：
+##### 使用工具：
 
 IntelliJ IDEA Ultimate (Student empowerment)
 
 MySQL 8.0
+
+
+
+##### 另一个配置文件需要配置:
+
+> pubkeyPath 	#RSA 8192公钥路径
+>
+> PrikeyPath     #RSA 8192私钥路径
+
+> host 		#邮箱的SMTP host
+>
+> uname 		#邮箱的用户名
+>
+> pwd 		#邮箱的密码
+>
+> from 		#发送邮件的邮箱全名
+>
+> subject 		#邮箱标题
+>
+> content 		#内容，需要带有一个{0}占位符
---
+++ src/cn/ovea/controller/web/servlet/TeacherCenterServlet.java
@@ -0,0 +1,46 @@
+package cn.ovea.controller.web.servlet;
+
+import cn.ovea.model.Teacher_information;
+import cn.ovea.tool.servlet.BaseServlet;
+
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/TeacherCenter")
+public class TeacherCenterServlet extends BaseServlet {
+    public String baseInfoChange(HttpServletRequest req, HttpServletResponse resp){
+        Teacher_information userInfo = (Teacher_information) req.getSession().getAttribute("userInfo");
+        String sid = req.getParameter("sid");
+        String name = req.getParameter("name");
+        String phoneNumber = req.getParameter("phoneNumber");
+        String qq = req.getParameter("qq");
+        String sex = req.getParameter("sex");
+
+        if(sid.equals("") || sid.length() != 4){
+            req.setAttribute("form", "职工号不满足规则（必须为4位）");
+            return "f:/Center";
+        }
+        userInfo.setStaff_id(sid);
+
+        if(!name.equals("")){
+            userInfo.setName(name);
+        }
+
+        if(!phoneNumber.equals("")){
+            userInfo.setPhone_number(phoneNumber);
+        }
+
+        if(!qq.equals("")){
+            userInfo.setQq(qq);
+        }
+
+        if(!sex.equals("true")){
+            userInfo.setSex(true);
+        }else {
+            userInfo.setSex(false);
+        }
+
+        return "f:/Center";
+    }
+}
